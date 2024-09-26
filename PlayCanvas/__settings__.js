263b1d6cdabbeb5d1e9ed93fc11ffc6797b7f135
jmill123 <170875141+jmill123@users.noreply.github.com>
2024-09-26 00:42:12 -0400

    Add PlayCanvas Files

---
+++ PlayCanvas/__settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2076180.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`, `webgpu`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 196987805 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
